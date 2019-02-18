f2c1a144c5cec87f14b9fcec92b32abb34cbd258
Keith P. Jolley <keithpjolley@gmail.com>
2019-02-17 19:54:55 -0800

    cleaning new code

--- configuration/terraform/main.tf
+++ configuration/terraform/main.tf
@@ -241,9 +241,13 @@ data "template_file" "gitlab_application_user_data" {
     nfs_server_private_ip = "${aws_instance.nfs_server.private_ip}"
     postgres_database     = "${aws_db_instance.gitlab_postgres.name}"
     postgres_username     = "${aws_db_instance.gitlab_postgres.username}"
-    postgres_password     = "${var.gitlab_postgres_password}"
+    postgres_password     = "${var.postgres_passwd}"
     postgres_endpoint     = "${aws_db_instance.gitlab_postgres.address}"
     redis_endpoint        = "${aws_elasticache_replication_group.gitlab_redis.primary_endpoint_address}"
+    key_name              = "${var.key_name}"
     cidr                  = "${module.vpc.cidr_block}"
+
+
+
   }
 }
--- configuration/terraform/modules/install_gitlab/autoscaling/main.tf
+++ configuration/terraform/modules/install_gitlab/autoscaling/main.tf
@@ -23,8 +23,8 @@ data "aws_ami" "gitlab_application_ami" {
 resource "aws_launch_configuration" "gitlab_application" {
   name_prefix     = "${var.name}-gitlab-application-"
   image_id        = "${var.gitlab_application_ami}"
-  instance_type   = "t2.large"
-  security_groups = ["${aws_security_group.gitlab_application.id}", "${module.security_groups.internal_ssh}"]
+  instance_type   = "${var.instance_type}"
+  security_groups = "${var.security_groups}"
   key_name        = "${var.key_name}"
   tags            = "${var.tags}"
   user_data       = "${data.template_file.gitlab_application_user_data.rendered}"
